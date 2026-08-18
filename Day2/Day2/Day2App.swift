42179a532dee741dd5e308aa237ebcf47b11d5ca
Navneet <navneet@gmail.com>
2026-08-18 15:16:00 +0530

    Day 2 Project Added

---
+++ Day2/Day2/Day2App.swift
@@ -0,0 +1,17 @@
+//
+//  Day2App.swift
+//  Day2
+//
+//  Created by HIMANK on 18/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Day2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
