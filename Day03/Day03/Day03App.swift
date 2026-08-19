304c46bd85e074cd7755ff2122a17cab33254849
Navneet <navneet@gmail.com>
2026-08-19 14:40:33 +0530

    Day - 03 Created

---
+++ Day03/Day03/Day03App.swift
@@ -0,0 +1,17 @@
+//
+//  Day03App.swift
+//  Day03
+//
+//  Created by HIMANK on 19/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Day03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Day03/Day03Tests/Day03Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Day03Tests.swift
+//  Day03Tests
+//
+//  Created by HIMANK on 19/08/26.
+//
+
+import Testing
+@testable import Day03
+
+struct Day03Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
